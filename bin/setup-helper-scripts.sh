ba2ba4b07df67cb5a0653a1ef81363c52d1e199d
Ale Feltes <afeltes@sodep.com.py>
2013-12-27 11:02:52 -0300

    Only run setup when HS_HOME is set.

--- bin/setup-helper-scripts.sh
+++ bin/setup-helper-scripts.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-. ../conf/vars
+HS_HOME=/opt/helper-scripts
 if [ -d $HOME/bin ]; then
 	mkdir -pv $HOME/bin
 fi
 echo "# START - helper-scripts stuff"
-echo "HS_HOME=$HS_HOME"
-export "HS_HOME"
-echo "PATH=\$PATH:\$HS_HOME/bin:\$HS_HOME/sbin"
-echo "export PATH HS_HOME"
-echo "\$HS_HOME/bin/backup-historial.sh > \$HOME/.historial-${UID}.log & "
-echo "# END - helper-scripts stuff"
+if [ $HS_HOME = "" ]; then
+	echo "HS_HOME not set. Exiting."
+	exit 1
+else
+	. $HS_HOME/conf/vars
+	echo "HS_HOME=$HS_HOME"
+	cd $HS_HOME/bin
+	export "HS_HOME"
+	echo "PATH=\$PATH:\$HS_HOME/bin:\$HS_HOME/sbin"
+	echo "export PATH HS_HOME"
+	echo "\$HS_HOME/bin/backup-historial.sh > \$HOME/.historial-${UID}.log & "
+	echo "# END - helper-scripts stuff"
+fi
 
 

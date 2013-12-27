411f6af292f0db7535fef169eea93a0ef8304075
Ale Feltes <afeltes@sodep.com.py>
2013-12-27 11:02:03 -0300

    Strips only strings from command line.

--- bin/backup-historial.sh
+++ bin/backup-historial.sh
@@ -14,6 +14,6 @@ cp -vf $HOME/.bash_history $DIR/history-${STAMP}
 cd $DIR
 echo "" > /tmp/history-global-$UID
 chmod -v 0600 /tmp/history-global-$UID
-cat * | sort | uniq   >> /tmp/history-global-$UID
+cat * | strings | sort | uniq   >> /tmp/history-global-$UID
 mv -vf /tmp/history-global-$UID .
 

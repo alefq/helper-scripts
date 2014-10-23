e2b914aabf912c4fc5373443d4cd9a19e6fb5ab8
Ale Feltes <afeltes@sodep.com.py>
2014-10-23 08:56:45 -0300

    Added script to organize files in a directory by: day, week or month. Suggestion, run from anacron: @monthly 15organize-files/opt/helper-scripts/bin/organize-files-by-time.sh m /path/to/dir

---
+++ bin/organize-files-by-time.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# put this in your logrotate conf 
+# whether you want the files to be organized by day, week or month
+#
+if [ $# - ne 2 ]; then
+	echo "Usage: $0 [d|w|m] [DIR TO ORGANIZE]"
+	echo "d: daily, w: weekly, m: monthly"
+	exit 1
+fi
+
+DIR_TO_ROTATE=$2
+OWNER="`stat -c '%U' DIR_TO_ROTATE`"
+NEW_DIR_MONTHLY=$DIR_TO_ROTATE/`date +%Y%m`
+NEW_DIR_WEEKLY_DAILY=$DIR_TO_ROTATE/`date +%Y%m%d`
+echo "$1"
+case "$1" in
+	d|D|daily)
+	NEW_DIR=$NEW_DIR_WEEKLY_DAILY
+	;;
+	w|W|weekly)
+	NEW_DIR=$NEW_DIR_WEEKLY_DAILY
+	;;
+	m|M|montlhy)
+	NEW_DIR=$NEW_DIR_MONTHLY
+	;;
+	*)
+	#default to daily
+	NEW_DIR=$NEW_DIR_WEEKLY_DAILY
+	;;
+esac
+mkdir -p $NEW_DIR
+
+cd $DIR_TO_ROTATE
+find . -maxdepth 1 -type f -exec mv -v -t $NEW_DIR {} + 
+chown -R $OWNER $NEW_DIR
+

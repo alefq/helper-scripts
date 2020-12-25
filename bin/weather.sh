cc126cdb53e9aaa5fc349652462cb48113e12861
Ale Feltes <afeltes@sodep.com.py>
2020-12-25 10:09:54 -0300

    Supports already existing DISPLAY variable

--- bin/weather.sh
+++ bin/weather.sh
@@ -2,10 +2,13 @@
 #
 # requires wethr from snap store and pango1.0-tools
 #
-# running from crontab
-export DISPLAY=:0
 WFILE=/tmp/.weather-info-$UID
 DEBUG=0
+
+if [ -z $DISPLAY ]; then
+	# probably running from crontab
+	export DISPLAY=:0
+fi
 
 if [ $DEBUG -eq 0 ]; then
 	/snap/bin/wethr > $WFILE 2>/dev/null
